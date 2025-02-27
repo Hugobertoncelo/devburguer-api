3e439c17c0e155db2c548805d9df9af89228538e
Hugobertoncelo <hugobertoncelo@gmail.com>
2025-02-27 08:11:43 -0300

    api products

--- src/app/controllers/UserController.ts
+++ src/app/controllers/UserController.ts
@@ -1,15 +1,45 @@
 import { v4 } from "uuid";
+import * as Yup from "yup";
 
 import User from "../models/User";
 
 class UserController {
   async store(req, res) {
-    const { name, email, password_hash, admin } = req.body;
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password: Yup.string().min(6).required(),
+      admin: Yup.boolean(),
+    });
+
+    try {
+      schema.validateSync(req.body, { abortEarly: false });
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        return res.status(400).json({ error: err.errors });
+      }
+      return res.status(500).json({ error: "Internal server error" });
+    }
+
+    const { name, email, password, admin } = req.body;
+
+    const userExists = await User.findOne({
+      where: {
+        email,
+      },
+    });
+
+    if (userExists) {
+      return res.status(400).json({ error: "User already exists" });
+    }
+
+    console.log(userExists);
+
     const user = await User.create({
       id: v4(),
       name,
       email,
-      password_hash,
+      password,
       admin,
     });
     return res.status(201).json({
--- src/database/index.ts
+++ src/database/index.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const configDatabase = require("../config/database");
+import configDatabase from "../config/database";
 
 import User from "../app/models/User";
 
